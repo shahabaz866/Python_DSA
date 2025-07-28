f203c2a1bfcffd463472a011acd43447836427f7
Muhammed shahabas VP <shahabaz866@gmail.com>
2025-07-29 03:25:03 +0530

    Add Day 2 workouts: python basic

--- workouts.py
+++ workouts.py
@@ -16,6 +16,106 @@
 ## insertion sort
 ##  find out the corerct position and insert the selected value in there.
 
-def insertion(arr):
+# def insertion(arr):
+#     n= len(arr)
+
+#     for i in range(1,n):
+
+#         j= i-1
+#         key = arr[i]
+#         while 
+
+
+
+# one = {
+#     1:"one",
+#     2:"two"
+# }
+
+# # one = "one"
+# two = "two"
+
+# # print(f'result{one.values()}',one)
+
+# print(one.())
+
+
+# person = {
+#     "name": "shibu",
+#     "age":7,
+#     'place':'pookutti'
+
+# }
+
+
+# def work(person):
+#     print(f'my name is {person.get('name')}')
+#     print(f'age is {person.get('age')}')
+
+# work(person)
+
+
+# dicta ={
+#     'A' : 10,
+#     'B' : 20,
+#     'C' : 5
+# }
+# maxi={
+#     'key':'','value':0
+#     }
+
+# for i,j in dicta.items():
+#     # print(i,j) 
+#     if maxi < j:
+#         maxi. = j
+
+
+# print(maxi,key)
+# vari = 9
+# def practice():
     
+#      vari = 1
+
+
+# practice()
+# print(vari)
+
+
+# HELLO = 9
+
+# HELLO = 10
+
+# print(HELLO)
+# class Constants:
+#     @property
+#     def PI(self):
+#         return 3.14
+
+# const = Constants()
+# print(const.PI)
+# const.PI = 3.15
+
+# import time
+
+# print("Start")
+# time.sleep(3)  # Waits for 3 seconds
+# print("End")
+
+# while 
+
+def gen(valu):
+    for i in range(1,valu+1):
+      print(i)
+      yield i
+
+res = gen(3)
+
+# for i in range(5):
+   
+print(next(res)) 
+print(next(res)) 
+print(next(res)) 
+print(next(res)) 
+
+# print(res.)
 
