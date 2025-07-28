dc85e07f67b9d4b264f1424be62f78e1de4a6c87
Muhammed shahabas VP <shahabaz866@gmail.com>
2025-07-29 00:11:20 +0530

    Add Day 2 workouts: insertion sort

---
+++ workouts.py
@@ -0,0 +1,21 @@
+# # bubble sort
+# # bubble sort means when we have to swap the value and 
+# def bubble(arr):
+#     n= len(arr)
+#     for i in range(n):
+#         for j in range(n-1):
+#             if arr[j] > arr[j+1]:
+#                 temp = arr[j]
+#                 arr[j] = arr[j+1]
+#                 arr[j+1] = temp
+    
+#     return arr
+
+# print(bubble([1,2,4,7,9,5,99,79]))
+
+## insertion sort
+##  find out the corerct position and insert the selected value in there.
+
+def insertion(arr):
+    
+
